88f5be59688ec50e9d366d70e0a3b47b69c5427f
Jessica <jessica.liang@outlook.com>
2016-09-25 02:26:19 -0400

    working form w/o map

--- app/src/main/java/com/diginals/microlancer/PostJob.java
+++ app/src/main/java/com/diginals/microlancer/PostJob.java
@@ -1,23 +1,35 @@
 package com.diginals.microlancer;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
-public class PostJob extends AppCompatActivity {
+public class PostJob extends AppCompatActivity implements View.OnClickListener{
     private Spinner spinner4;
     private Button button3;
     private Spinner spinner6;
 
 
+    private ImageButton ib;
+    private Calendar cal;
+    private int day;
+    private int month;
+    private int year;
+    private EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +37,34 @@ public class PostJob extends AppCompatActivity {
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         addListenerOnButton();
         addListenerOnSpinnerItemSelection();
+
+        ib = (ImageButton) findViewById(R.id.imageButton1);
+        cal = Calendar.getInstance();
+        day = cal.get(Calendar.DAY_OF_MONTH);
+        month = cal.get(Calendar.MONTH);
+        year = cal.get(Calendar.YEAR);
+        et = (EditText) findViewById(R.id.editText);
+        ib.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        showDialog(0);
+    }
+
+    @Override
+    @Deprecated
+    protected Dialog onCreateDialog(int id) {
+        return new DatePickerDialog(this, datePickerListener, year, month, day);
+    }
+
+    private DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+            et.setText(selectedDay + " / " + (selectedMonth + 1) + " / "
+                    + selectedYear);
+        }
+    };
 
 
     public void addListenerOnSpinnerItemSelection() {
